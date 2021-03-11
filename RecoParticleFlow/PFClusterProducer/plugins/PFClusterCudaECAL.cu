fee2be66663d78b15d5a04fa40f98657207c5448
Jakob <jakob.salfeld@cern.ch>
2021-03-11 15:40:34 +0100

    new topo clustering for ECAL

--- RecoParticleFlow/PFClusterProducer/plugins/PFClusterCudaECAL.cu
+++ RecoParticleFlow/PFClusterProducer/plugins/PFClusterCudaECAL.cu
@@ -91,10 +91,42 @@ namespace PFClusterCudaECAL {
       
     }//loop over neumann neighbourhood clustering end
   }
+
+  __global__ void topoKernel_ECALV2( 
+				  size_t size,
+				  float* pfrh_energy,
+				  int* pfrh_topoId,
+				  int* pfrh_layer,
+				  int* neigh8_Ind
+				  ) {
+
+      int l = threadIdx.x+blockIdx.x*blockDim.x;
+      int k = threadIdx.y+blockIdx.y*blockDim.y;
+
+      if(l<size && k<8) {	
+	  while( neigh8_Ind[nNeigh*l+k] > -1 && pfrh_topoId[l] != pfrh_topoId[neigh8_Ind[nNeigh*l+k]] && 
+		 (
+		  ((pfrh_layer[l] == -2 && pfrh_energy[l]>topoEThresholdEE) || 
+		   (pfrh_layer[l] == -1 && pfrh_energy[l]>topoEThresholdEB) )
+		  &&
+		  ((pfrh_layer[neigh8_Ind[nNeigh*l+k]] == -2 && pfrh_energy[neigh8_Ind[nNeigh*l+k]]>topoEThresholdEE) || 
+		   (pfrh_layer[neigh8_Ind[nNeigh*l+k]] == -1 && pfrh_energy[neigh8_Ind[nNeigh*l+k]]>topoEThresholdEB) )
+		  )
+		 )
+	    {
+	      if(pfrh_topoId[l] > pfrh_topoId[neigh8_Ind[nNeigh*l+k]]){
+		atomicMax(&pfrh_topoId[neigh8_Ind[nNeigh*l+k]],pfrh_topoId[l]);
+	      }
+	      if(pfrh_topoId[l] < pfrh_topoId[neigh8_Ind[nNeigh*l+k]]){
+		atomicMax(&pfrh_topoId[l], pfrh_topoId[neigh8_Ind[nNeigh*l+k]]);
+	      }	      
+	    }
+      }				           
+  }
   
-
-
-__global__ void fastCluster_step1( size_t size,
+  
+      
+      __global__ void fastCluster_step1( size_t size,
 					     float* pfrh_x,
 					     float* pfrh_y,
 					     float* pfrh_z,
@@ -211,20 +243,26 @@ __global__ void fastCluster_step2( size_t size,
     //cudaDeviceSynchronize();
       
     // for(int a=0;a<16;a++){
-    if(size>0) topoKernel_ECAL<<<(size+512-1)/512, 512>>>( size, pfrh_energy,  pfrh_topoId,  pfrh_layer, neigh8_Ind);
+    //if(size>0) topoKernel_ECAL<<<(size+512-1)/512, 512>>>( size, pfrh_energy,  pfrh_topoId,  pfrh_layer, neigh8_Ind);
     //}	    
-    cudaDeviceSynchronize();
+    //cudaDeviceSynchronize();
 
+    dim3 gridT( (size+64-1)/64, 1 );
+    dim3 blockT( 64, 8);
+    for(int h=0;h<18; h++){  
+      if(size>0) topoKernel_ECALV2<<<gridT, blockT>>>( size, pfrh_energy,  pfrh_topoId,  pfrh_layer, neigh8_Ind);	     
+    }
+    
     dim3 grid( (size+32-1)/32, (size+32-1)/32 );
     dim3 block( 32, 32);
 
     //if(size>0) std::cout<<std::endl<<"NEW EVENT !!"<<std::endl<<std::endl;
 
      if(size>0) fastCluster_step1<<<grid, block>>>( size, pfrh_x,  pfrh_y,  pfrh_z,  pfrh_energy, pfrh_topoId,  pfrh_isSeed,  pfrh_layer, pcrhfrac, pcrhfracind, fracSum, rhCount);
-     cudaDeviceSynchronize();
+     //cudaDeviceSynchronize();
 
     if(size>0) fastCluster_step2<<<grid, block>>>( size, pfrh_x,  pfrh_y,  pfrh_z,  pfrh_energy, pfrh_topoId,  pfrh_isSeed,  pfrh_layer, pcrhfrac, pcrhfracind, fracSum, rhCount);  
-    cudaDeviceSynchronize();
+    //cudaDeviceSynchronize();
     
    
   }
